410be143ae9d06228479f50508c01356357fc9c6
emhart <edmund.m.hart@gmail.com>
2014-04-16 17:53:41 -0700

    Still fixing vignette stuff for the push to cran.

--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@ all: pandoc move
 
 pandoc:
 	cd inst/stuff/;\
-pandoc -H margins.sty rWBclimate_vignette.md -o rWBclimate_vignette.pdf --highlight-style=tango;\
+pandoc -H margins.sty rWBclimate_vignette.md -o rWBclimate.pdf --highlight-style=tango;\
 
 move:
-	mv inst/stuff/rWBclimate_vignette.pdf inst/stuff/vignette.pdf
-	cp inst/stuff/*.pdf vignettes;\
+	cp inst/stuff/*.pdf ../doc;\
   
--- R/rWBclimate-package.r
+++ R/rWBclimate-package.r
@@ -15,3 +15,69 @@ NULL
 #' @docType data
 #' @keywords datasets
 NULL
+
+#' Country codes for all of Africa
+#' @name Africa_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Asia, used in downloading maps
+#' @name Asia_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Asia
+#' @name Asia_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Eur, used in downloading maps
+#' @name Eur_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Eur
+#' @name Eur_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for NoAm, used in downloading maps
+#' @name NoAm_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of NoAm
+#' @name NoAm_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Oceana, used in downloading maps
+#' @name Oceana_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Oceana
+#' @name Oceana_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for SoAm, used in downloading maps
+#' @name SoAm_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of SoAm
+#' @name SoAm_country
+#' @docType data
+#' @keywords datasets
+NULL
