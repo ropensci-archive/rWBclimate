9c5c3baa0553a651249e2f87a05ce1c9f93c2f3d
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-30 14:18:04 -0700

    added datasets var names to globals to ignore

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 if (base::getRversion() >= "2.15.1") {
-  utils::globalVariables(c("long", "lat","group")) ## Neded in functions plot_density and plot_signposts
+  utils::globalVariables(c("long", "lat","group","NoAm_country","SoAm_country","Oceana_country","Africa_country","Asia_country","Eur_country"))
 }
